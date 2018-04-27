bbcda4036e571e3542ce599fd11c8963e47d159a
Rabauke84 <v.gierend@googlemail.com>
2018-04-27 14:30:18 +0200

    SELECT Anweisung

--- select.sql
+++ select.sql
@@ -1,1 +1,13 @@
 USE [dotnetconsulting_Videogames];
+
+select * from dbo.Spieler;
+
+select * from Spieler;
+
+SELECT Name AS 'Spielername',
+       Nickname AS 'Spitzname'
+FROM    dbo.Spieler;
+
+SELECT Name + ' (' + Nickname +  ')' AS 'Name des Spielers' FROM Spieler;
+
+SELECT SUBSTRING(Name,1,2) FROM Spieler;
