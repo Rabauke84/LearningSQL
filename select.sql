540dc3d839186b5a10cded45c76be1c95b93b862
Vince <v.gierend@googlemail.com>
2018-04-28 15:22:21 +0200

    SELECT

--- select.sql
+++ select.sql
@@ -1,1 +1,10 @@
 use dotnetconsulting_Videogames;
+
+SELECT * FROM Spieler;
+
+SELECT Name as 'Spielername',
+    Nickname as 'Nick'
+FROM Spieler;
+
+Select Name + ' (' + 'Nickname' + ')' as Spieler
+FROM Spieler;
