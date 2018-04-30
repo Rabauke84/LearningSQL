947f39a1ab17ae0e7a0f9e84657747bb6eb7366d
Rabauke84 <v.gierend@googlemail.com>
2018-04-30 15:36:12 +0200

    Delete Queries

--- SELECT/group.sql
+++ SELECT/group.sql
@@ -4,9 +4,3 @@ use GMW_TEST;
 SELECT Name, COUNT(*) as Anzahl FROM Spieler
     GROUP BY Name
     ORDER BY Anzahl DESC;
-
-use ps_dat;
-SELECT tNuName,tNuVorname, COUNT(*) as 'Anzahl'
-    FROM tNutzerAdmin
-    GROUP BY tNuName, tNuVorname
-    HAVING COUNT(*) > 1;
